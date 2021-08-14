56c20e61f888b134e03906c37c1bb08bfbca155c
Lelianto Eko Pradana <lelianto.pradana@rupiahcepat.co.id>
2021-08-14 16:59:20 +0700

    final product

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Product {
-  final String? image, title, description;
-  final int? price, size, id;
-  final Color? color;
+  final String image, title, description;
+  final int price, size, id;
+  final Color color;
   Product({
-    this.id,
-    this.image,
-    this.title,
-    this.price,
-    this.description,
-    this.size,
-    this.color,
+    required this.id,
+    required this.image,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.size,
+    required this.color,
   });
 }
 
--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/constants.dart';
+import 'package:my_first_app/models/Product.dart';
+import 'package:my_first_app/screens/details/details_screen.dart';
+
+import 'categories.dart';
+import 'item_card.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -8,73 +13,40 @@ class Body extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+        Expanded(
+          child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-            child: Text(
-              'Women',
-              style: Theme.of(context)
-                  .textTheme
-                  .headline5!
-                  .copyWith(fontWeight: FontWeight.bold),
-            )),
-        Categories()
-      ],
-    );
-  }
-}
-
-class Categories extends StatefulWidget {
-  @override
-  _CategoriesState createState() => _CategoriesState();
-}
-
-class _CategoriesState extends State<Categories> {
-  List<String> categories = ["Hand bag", "Jewellery", "Footwear", "Dresses"];
-
-  int selectedIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: kDefaultPaddin),
-      child: SizedBox(
-          height: 25,
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) => buildCategory(index)))
-    );
-  }
-
-  Widget buildCategory(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedIndex = index;
-        });
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-                Text(
-                  categories[index],
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: selectedIndex == index
-                          ? kTextColor
-                          : kTextLightColor),
+            child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: kDefaultPaddin,
+                  crossAxisSpacing: kDefaultPaddin,
+                  childAspectRatio: 0.75,
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: kDefaultPaddin / 4),
-                  height: 2,
-                  width: 20,
-                  color: selectedIndex == index
-                      ? Colors.black
-                      : Colors.transparent,
-                )
-              ]
-        )
-      )
+                itemBuilder: (context, index) => ItemCard(
+                      product: products[index],
+                      press: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailsScreen(
+                              product: products[index],
+                            ),
+                          )),
+                    )),
+          ),
+        ),
+      ],
     );
   }
 }
